797d60bf769560d6459dffbcc4177990a00746e3
Mirko Seifert <mirko.seifert@devboost.de>
2013-03-30 20:57:51 +0100

    added PluginTestHelper class containing shared test code

---
+++ de.devboost.testutils/src/de/devboost/testutils/PluginTestHelper.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2013
+ * Software Technology Group, Dresden University of Technology
+ * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany;
+ *   DevBoost GmbH - Berlin, Germany
+ *      - initial API and implementation
+ ******************************************************************************/
+package de.devboost.testutils;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ * The {@link PluginTestHelper} can be used to access test resources (e.g., test
+ * data) using the location of a Java class instead of the current working
+ * directory.
+ */
+public class PluginTestHelper {
+
+	public String getPluginRootPath(Class<?> clazz) {
+		URL location = clazz.getProtectionDomain().getCodeSource().getLocation();
+		String binFolder = location.getFile().replace("%20", " ");
+		String rootPathString;
+		// we must not use File.separator here as the path is obtained from an
+		// URL which uses forward slashes on all OSs
+		if (binFolder.endsWith("/bin/")) {
+			rootPathString = binFolder + File.separator + "..";
+		} else {
+			rootPathString = binFolder;
+		}
+		String pluginRootPath = new File(rootPathString).getAbsolutePath();
+		return pluginRootPath;
+	}
+
+	public String getSourcePackagePath(Class<?> clazz) {
+		String pluginRootPath = getPluginRootPath(clazz);
+		String packageDirectory = File.separator + "src" + File.separator + clazz.getPackage().getName().replace(".", File.separator) + File.separator;
+		String sourcePackagePath = pluginRootPath + packageDirectory;
+		return sourcePackagePath;
+	}
+}
---
+++ de.devboost.testutils/src/de/devboost/testutils/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains a set of classes that are used by different DevBoost
+ * tools for automated testing.
+ */
+package de.devboost.testutils;
